6ac3652a95f8a620dcd796d5c04de3eeba08966a
Jheferson Checa <124943149+JhefersonCh@users.noreply.github.com>
2025-10-28 23:42:14 -0500

    Feat/implements movies module (#4)
    * ✨ feat: Add movies module
    
    This commit introduces the movies module, which includes the following features:
    - CRUD operations for movies.
    - Pagination for the movie list.
    - Search movies by title.
    
    * :recycle: Refactor: docker compose to local and dev with deployed dbs and adjust conections with this dbs

---
+++ src/main/java/com/cineclub_backend/cineclub_backend/config/MongoConfig.java
@@ -0,0 +1,12 @@
+package com.cineclub_backend.cineclub_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = {
+    "com.cineclub_backend.cineclub_backend.movies.repositories",
+    "com.cineclub_backend.cineclub_backend.users.repositories"
+})
+public class MongoConfig {
+}
--- src/main/java/com/cineclub_backend/cineclub_backend/config/RedisConfig.java
+++ src/main/java/com/cineclub_backend/cineclub_backend/config/RedisConfig.java
@@ -3,7 +3,6 @@ package com.cineclub_backend.cineclub_backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -11,20 +10,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
-    }
-
-    @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
-
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new StringRedisSerializer());
-
         return template;
     }
 }
